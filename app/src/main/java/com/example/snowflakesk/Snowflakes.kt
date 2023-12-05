adb8cbb7bc125f7de7d713d859e6f7a89188bd5c
Ivan Petrushin <ivan.kiel@gmail.com>
2023-12-05 09:31:01 +0800

    colored skowflakes; movetask

--- app/src/main/java/com/example/snowflakesk/Snowflakes.kt
+++ app/src/main/java/com/example/snowflakesk/Snowflakes.kt
@@ -4,24 +4,27 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.AsyncTask
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import kotlin.random.Random
 
 // TODO: сделать снежинки различных оттенков, для чего добавить поле "цвет"
-data class Snowflake(var x: Float, var y: Float, val velocity: Float, val radius: Float)
+data class Snowflake(var x: Float, var y: Float, val velocity: Float, val radius: Float, val color: Int)
 lateinit var snow: Array<Snowflake>
 val paint = Paint()
 var h = 1000; var w = 1000 // значения по умолчанию, будут заменены в onLayout()
 
-class Snowflakes(ctx: Context) : View(ctx) {
+open class Snowflakes(ctx: Context) : View(ctx) {
+    lateinit var moveTask: MoveTask
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // перерисовывает канву
         canvas.drawColor(Color.BLUE)
-        paint.setColor(Color.WHITE)
+
         for (s in snow) {
+            paint.color = s.color
             canvas.drawCircle(s.x, s.y, s.radius, paint)
         }
 
@@ -33,12 +36,14 @@ class Snowflakes(ctx: Context) : View(ctx) {
         // TODO заполнить массив снежинками со случайными координатами, радиусами и скоростями
         val r = Random(0)
         r.nextFloat() // генерирует число от 0 до 1
-        snow = Array(10) { Snowflake(r.nextFloat() * w, r.nextFloat() * h,
-            15 + 10 * r.nextFloat(), 30 + 20 * r.nextFloat()) }
+        snow = Array(10) { Snowflake(x = r.nextFloat() * w, y =  r.nextFloat() * h,
+            velocity = 15 + 10 * r.nextFloat(), radius = 30 + 20 * r.nextFloat(),
+            Color.rgb(255F,255F,255F)) // TODO: сделать снежинки разного оттенка
+        }
         Log.d("mytag", "snow: " + snow.contentToString())
     }
 
-    fun MoveSnowflakes() {
+    fun moveSnowflakes() {
         // TODO: сделать падение с замедлением, к нижней границе экрана
         // скорость должна снижаться до нуля
         for (s in snow) {
@@ -47,12 +52,26 @@ class Snowflakes(ctx: Context) : View(ctx) {
                 s.y -= h // если снежинка покинула экран, вычитаем высоту
             }
         }
+        invalidate() // перерисовать
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        MoveSnowflakes()
-        invalidate() // перерисовать View
-        return true
+        //moveSnowflakes()
+        //invalidate() // перерисовать View
+        moveTask = MoveTask(this)
+        moveTask.execute(100)
+        return false // защита от множественных срабатываний
 
+    }
+    class MoveTask(val s: Snowflakes) : AsyncTask<Int,Int,Int>() {
+    // https://developer.alexanderklimov.ru/android/theory/asynctask.php
+        override fun doInBackground(vararg params: Int?): Int {
+            val delay = params[0] ?: 200 // если задерка не задана
+            while (true) {
+                Thread.sleep(delay.toLong())
+                s.moveSnowflakes()
+            }
+            return 0
+        }
     }
 }
